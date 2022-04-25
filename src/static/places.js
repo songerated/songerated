c1457e630e317339cbd3a3386ebeeb220f3f16e4
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-04-26 02:11:43 +0530

    Landing Page done

---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: 'Find friends you can relate with',
+    description:
+      " Aliqua incididunt sint ut adipisicing et enim incididunt. Ex sit ex est dolor et incididunt aute ipsum aute dolore. Do consequat minim quis laboris officia sit proident sint fugiat adipisicing qui incididunt.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/friends_music_1.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Get matched based on your spotify playlist',
+    description:
+    "Non mollit tempor ex ipsum consequat aliqua ad. Excepteur officia in esse ad consectetur eiusmod id. Aliqua in in id fugiat eu nostrud veniam non. Sint eiusmod dolor excepteur non deserunt et nulla Lorem non occaecat.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/spotify_2.jpg',
+    time: 1500,
+  },
+];
+
+export default places;
