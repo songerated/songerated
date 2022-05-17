f2e2120db6bee1a91bd74fd002628f8cd11c8f36
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-05-18 02:09:36 +0530

    added router

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
 import PlaceToVisit from './components/PlaceToVisit';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './Home';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,14 +12,15 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
   },
 }));
-export default function App() {
-  const classes = useStyles();
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+export default function Home() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Header />
+            <PlaceToVisit />
+        </div>
+    );
 }
