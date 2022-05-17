06578a305c7cecdf14bee96deab8ff3137a606e6
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-05-18 02:23:29 +0530

    added spotify link page

---
+++ src/SpotifyLink.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import PlaceToVisit from './components/PlaceToVisit';
+
+
+
+export default function SpotifyLink() {
+
+    return (
+        <div >
+            <h1>Hello</h1>
+        </div>
+    );
+}
