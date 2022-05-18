c9e7a1a27435688d170e7600e55845b214d4f3a8
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-05-18 19:35:01 +0530

    UI basics done + App bar added

--- src/SpotifyLink.js
+++ src/SpotifyLink.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import { CssBaseline } from '@material-ui/core';
-import Header from './components/Header';
-import PlaceToVisit from './components/PlaceToVisit';
-
-
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 export default function SpotifyLink() {
 
     return (
         <div >
-            <h1>Hello</h1>
+            <CssBaseline />
+            <ResponsiveAppBar />
+            <Container maxWidth="xs" >
+                <Box sx={{margin:'16px', padding: '16px', borderRadius: '8px', border: 1, borderColor: 'grey.500'}}>
+                    <center>
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Username" 
+                        variant="outlined"
+                        helperText="Your Spotify Username"
+                    />
+
+                    <Box sx={{padding: '16px'}}>
+                        <center>
+                            <Button variant="contained">Submit</Button>
+                        </center>
+                    </Box>
+                    </center>
+                </Box>
+            </Container>
+
         </div>
     );
 }
