4058ca8188aad08be3aed1e2a2ef1c7a9241aa90
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-05-21 15:00:05 +0530

    fixed spotify bug

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -25,12 +25,11 @@ export default function UserInfo()  {
       const hash = window.location.hash
       let token = window.localStorage.getItem("token")
   
-      if (!token && hash) {
           token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
   
           window.location.hash = ""
           window.localStorage.setItem("token", token)
-      }
+      
   
       setToken(token)
   
