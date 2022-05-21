88dd0306171165fcdfa68beffbbe644669bdb10c
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-05-22 02:19:48 +0530

    added user top artists

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,7 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import ResponsiveAppBar from './ResponsiveAppBar';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
-
+import Button from '@mui/material/Button';
+import Card from 'react-bootstrap/Card'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,6 +20,14 @@ const useStyles = makeStyles((theme) => ({
     multilineColor: {
         color: "green"
       }
+  }));
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   }));
 export default function UserInfo()  {
     const classes = useStyles();
@@ -36,20 +49,6 @@ export default function UserInfo()  {
   
     }, [])
 
-    const searchArtists = async (e) => {
-      e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
-          headers: {
-              Authorization: `Bearer ${token}`
-          },
-          params: {
-              q: searchKey,
-              type: "artist"
-          }
-      })
-
-      setArtists(data.artists.items)
-    }
 
     const getTopArtists = async (e) => {
       e.preventDefault()
@@ -63,39 +62,63 @@ export default function UserInfo()  {
           
       })
 
-      //setTopArtists(data.items)
-      renderTopArtists(data)
+      setTopArtists(data.items)
 
     }
 
-    const [searchKey, setSearchKey] = useState("")
-    const [artists, setArtists] = useState([])
     const [topArtists, setTopArtists] = useState([])
 
-    const renderArtists = () => {
-      return artists.map(artist => (
-          <div key={artist.id}>
-              {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-              {artist.name}
-          </div>
-      ))
-    }
 
-    const renderTopArtists = (data) => {
-        console.log(data)
+    const renderTopArtists = () => {
+        return topArtists.map(artist => (
+          <div key={artist.id}>
+            <Card style={{ width: '18rem', margin: '16px' }}>
+              <Card.Img variant="top" src={artist.images[0].url} />
+              <Card.Body>
+              <Card.Title>{artist.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Followers: {artist.followers.total}</Card.Subtitle>
+              <Card.Text>
+                <b>Genres:</b> {artist.genres.join(", ")}
+              </Card.Text>
+             
+            </Card.Body>
+            </Card>
+            
+            </div>
+        ))
     }
 
 
     return (
       <div className={classes.root}>
           <ResponsiveAppBar/>
-          <form onSubmit={searchArtists}>
-            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-            <button type={"submit"} onClick={renderArtists}>Search</button>
-            {renderArtists()}
-
-          </form>
-          <button onClick={getTopArtists}>Get Artist</button>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={9}>
+              <Grid item xs>
+                <Item>
+                <form onSubmit={getTopArtists}>
+                  <Button type={"submit"} onClick={renderTopArtists}>Get your spotify data</Button>
+                  {renderTopArtists()}
+                </form>
+                </Item>
+              </Grid>
+              <Grid item xs>
+                <Item>
+                <form onSubmit={getTopArtists}>
+                  <Button type={"submit"} onClick={renderTopArtists}>Get your spotify data</Button>
+                  {renderTopArtists()}
+                </form>
+                </Item>
+              </Grid>
+              <Grid item xs>
+                <Item>xs=4</Item>
+              </Grid>
+              
+            </Grid>
+        </Box>
+          <center>
+            
+          </center>
       </div>
     )
 }
