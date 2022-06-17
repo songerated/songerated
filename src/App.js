b08e9a2cf02432c4c4f5ecec44d4f6fd61732323
AprameyaL <lavaaprameya@gmail.com>
2022-06-17 19:13:11 +0530

    aditya sucks

--- src/App.js
+++ src/App.js
@@ -4,38 +4,25 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Home';
 import SpotifyLink from './SpotifyLink';
 import SignUp from './components/SignUp';
-<<<<<<< HEAD
 import { AuthProvider } from './contexts/authContexts';
-=======
 import UserInfo from './components/UserInfo';
 import ConnectSpotify from './components/ConnectSpotify';
->>>>>>> 4057d840f971fef8da5a2cbf863bb5546816b94b
 
 export default function App() {
 
   return (
-<<<<<<< HEAD
-    
+    <AuthProvider>
+
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/spotifylink" element={<SpotifyLink />} />
-          <Route path="/signup" element={<AuthProvider><SignUp /></AuthProvider>} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/userinfo" element={<UserInfo />} />
+          <Route path="/connectspotify" element={<ConnectSpotify />} />
+
         </Routes>
       </Router>
-    
-=======
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/spotifylink" element={<SpotifyLink />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/userinfo" element={<UserInfo />} />
-        <Route path="/connectspotify" element={<ConnectSpotify />} />
-
-      </Routes>
-    </Router>
->>>>>>> 4057d840f971fef8da5a2cbf863bb5546816b94b
+    </AuthProvider>
   );
 }
