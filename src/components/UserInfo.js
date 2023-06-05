7d5eece1e311cf91b914ebdac1151cef9a84906d
aadityaguptaa <rockonman3@gmail.com>
2023-06-05 14:11:38 +0530

    made some ui changes

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -17,7 +17,10 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Link, useNavigate } from "react-router-dom";
 import StepperComponent from "./StepperComponent";
-import { useAuth } from "../contexts/authContexts"
+import { useAuth } from "../contexts/authContexts";
+import { TokenOutlined } from "@mui/icons-material";
+import Skeleton from '@mui/material/Skeleton';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -56,29 +59,73 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 export default function UserInfo() {
   const [token, setToken] = useState("");
   const spotify_url = process.env.REACT_APP_SPOTIFY_BASE_URL;
-  const {currentUser} = useAuth() 
-  const id = currentUser.uid
+  const { currentUser } = useAuth();
+  const id = currentUser.uid;
   const navigate = useNavigate();
-  const classes = useStyles()
-  const server_base_url = process.env.REACT_APP_SERVER_URL
+  const classes = useStyles();
+  const server_base_url = process.env.REACT_APP_SERVER_URL;
 
   //This is the function that gets the token from the URL
   useEffect(() => {
     const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    let tokent = window.localStorage.getItem("token");
 
-    token = hash
+    tokent = hash
       .substring(1)
       .split("&")
       .find((elem) => elem.startsWith("access_token"))
       .split("=")[1];
 
     window.location.hash = "";
-    window.localStorage.setItem("token", token);
+    window.localStorage.setItem("token", tokent)
+    setToken(tokent);
+    console.log(tokent);
+    
+    
 
-    setToken(token);
-  }, []);
+  },[]);
 
+  useEffect(() => {
+    if (token === "")
+      return;
+    loadTracks();
+    console.log(token);
+    loadArtists();
+  }, [token])
+
+ 
+
+  const loadTracks = async (e) => {
+    console.log(token)
+    axios
+      .get(spotify_url + "/me/top/tracks", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {},
+      })
+      .then((response) => {
+        setTopTracks(response.data.items);
+        setIsTopTracks(true)
+        console.log(response.data.items);
+      });
+  }
+
+  const loadArtists = async (e) => {
+    console.log(token)
+    axios
+      .get(spotify_url + "/me/top/artists", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {},
+      })
+      .then((response) => {
+        setTopArtists(response.data.items);
+        setIsTopArtists(true)
+        console.log(response.data.items);
+      });
+  }
 
   const getTopArtists = async (e) => {
     e.preventDefault();
@@ -93,21 +140,10 @@ export default function UserInfo() {
     setTopArtists(data.items);
   };
 
-  const getTopTracks = async (e) => {
-    e.preventDefault();
-    const { data } = await axios.get(spotify_url + "/me/top/tracks", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params: {},
-    });
-    console.log(data);
-
-    setTopTracks(data.items);
-  };
-
   const [topArtists, setTopArtists] = useState([]);
   const [topTracks, setTopTracks] = useState([]);
+  const [isTopTracks, setIsTopTracks] = useState(false);
+  const [isTopArtists, setIsTopArtists] = useState(false);
 
   const renderTopArtists = () => {
     return (
@@ -122,7 +158,7 @@ export default function UserInfo() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {topArtists.map((artist) => (
+            {topArtists?.map((artist) => (
               <StyledTableRow key={artist.id}>
                 <StyledTableCell component="th" scope="row">
                   {artist.name}
@@ -145,63 +181,25 @@ export default function UserInfo() {
   };
 
   function handleOnSubmit() {
-    axios.get(server_base_url + "/verifyuser", {
-
-      params: {
-        id: id
-      }
-    }).then(res => {
-      if(res.data.length === 0){
-        axios.post(server_base_url + "/tracks" , { topTracks: topTracks, uid: id })
-        .then(response =>     navigate("/addmovies")
-        );
-
-      }else{
-        navigate("/addmovies")
-      }
-
-    
-    })
+    axios
+      .get(server_base_url + "/verifyuser", {
+        params: {
+          id: id,
+        },
+      })
+      .then((res) => {
+        if (res.data.length === 0) {
+          axios
+            .post(server_base_url + "/tracks", {
+              topTracks: topTracks,
+              uid: id,
+            })
+            .then((response) => navigate("/addmovies"));
+        } else {
+          navigate("/addmovies");
+        }
+      });
   }
-
-  const renderTopTracks = () => {
-    return (
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 700 }} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Track Name</StyledTableCell>
-              <StyledTableCell align="right">Artists</StyledTableCell>
-              <StyledTableCell align="right">Album</StyledTableCell>
-              <StyledTableCell align="right">Popularity</StyledTableCell>
-              <StyledTableCell align="right">Explicit</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {topTracks.map((track) => (
-              <StyledTableRow key={track.id}>
-                <StyledTableCell component="th" scope="row">
-                  {track.name}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                  {track.artists.map((artist) => artist.name).join(", ")}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                  {track.album.name}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                  {track.popularity}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                  {track.explicit.toString()}
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  };
 
   return (
     <div className={classes.root}>
@@ -210,30 +208,73 @@ export default function UserInfo() {
       </div>
       <center>
         <Box sx={{ margin: "32px", width: "80%" }}>
-          <form onSubmit={getTopTracks}>
-            <Button
-              type={"submit"}
-              variant="contained"
-              sx={{ bgcolor: "black", margin: "16px" }}
-              onClick={renderTopTracks}
-            >
-              Get your most listened songs
-            </Button>
-            {renderTopTracks()}
-          </form>
+          {
+            !isTopTracks && <Grid container spacing={8}>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+                          <Skeleton width="100%"/>
+
+          </Grid>
+          }
+            {
+              isTopTracks && <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Track Name</StyledTableCell>
+                    <StyledTableCell align="right">Artists</StyledTableCell>
+                    <StyledTableCell align="right">Album</StyledTableCell>
+                    <StyledTableCell align="right">Popularity</StyledTableCell>
+                    <StyledTableCell align="right">Explicit</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {topTracks?.map((track) => (
+                    <StyledTableRow key={track.id}>
+                      <StyledTableCell component="th" scope="row">
+                        {track.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {track.artists.map((artist) => artist.name).join(", ")}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {track.album.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {track.popularity}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {track.explicit.toString()}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            }
+          
+
+          {/* </form> */}
         </Box>
         <Box sx={{ margin: "32px", width: "80%" }}>
-          <form onSubmit={getTopArtists}>
-            <Button
-              type={"submit"}
-              variant="contained"
-              sx={{ bgcolor: "black", margin: "16px" }}
-              onClick={renderTopArtists}
-            >
-              Get your favourite artists
-            </Button>
-            {renderTopArtists()}
-          </form>
+          
+          {isTopArtists && renderTopArtists()}
         </Box>
 
         <Box sx={{ margin: "32px", width: "80%" }}>
